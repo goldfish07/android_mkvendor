9363261df800fcfd88ce648e9028b7f481da7dff
Fish <ayushbisht5663@gmail.com>
2019-10-10 19:39:30 +0530

    refract::git

--- device/mkvendor.sh
+++ device/mkvendor.sh
@@ -103,11 +103,5 @@ mv $DEVICE_DIR/device.mk $DEVICE_DIR/device_$DEVICE.mk
 
 echo Creating initial git repository.
 pushd $DEVICE_DIR
-git init
-git add .
-git commit -a -m "mkvendor.sh: Initial commit of $DEVICE"
 popd
-
 echo Done!
-echo Use the following command to set up your build environment:
-echo '  'lunch cm_$DEVICE-eng
